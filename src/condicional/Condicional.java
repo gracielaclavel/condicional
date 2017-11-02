f55a070d72b98a013fd7332edf2229871cf758bd
LAB18 <LAB18@DESKTOP-0RAG06Q>
2017-11-02 19:59:36 -0300

    inicializacion del repositorio y creacion de clases

---
+++ src/condicional/Condicional.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condicional;
+
+/**
+ *
+ * @author LAB18
+ */
+public class Condicional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
