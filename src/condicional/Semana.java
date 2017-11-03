6ed30580df63210d5f45c592bf2f74a7e92babf6
LAB18 <LAB18@DESKTOP-0RAG06Q>
2017-11-03 20:55:18 -0300

    Este programa es mas facil

--- src/condicional/Semana.java
+++ src/condicional/Semana.java
@@ -10,6 +10,45 @@ import javax.swing.JOptionPane;
  *
  * @author LAB18
  */
-public class Semana {
+public class Semana 
+{
+  public static void main(String[] args) 
+    {
+    float numero = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el numero "));
+     switch(Math.round(numero))
+     {
+     case 1://Ingresando dias
+             JOptionPane.showMessageDialog(null,"Lunes");
+             
+         break;
+          case 2://agregando otro dias
+             JOptionPane.showMessageDialog(null,"Martes");
+             
+         break;
+          case 3://agregando otro dias
+             JOptionPane.showMessageDialog(null,"Miercoles");
+             
+         break;
+          case 4://agregando otro dias
+             JOptionPane.showMessageDialog(null,"Jueves");
+             
+         break;
+           case 5://agregando otro dias
+             JOptionPane.showMessageDialog(null,"Viernes");
+             
+         break;
+          case 6://Agregando dias
+             JOptionPane.showMessageDialog(null,"Sabado");
+             
+         break;
+          case 7:
+             JOptionPane.showMessageDialog(null,"Domingo");
+             
+         break;
+          
+              
+     }
+          
     
+    }
 }
